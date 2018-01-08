34cc25681acf0246cc2069bd13c33c0c81b37475
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-01-08 16:37:34 +0530

    Create sugan.c

---
+++ sugan.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",\n);
+if(n>=0)
+{
+printf("given number is positive");
+}
+else
+{
+printf("given number is negative");
+}
+return 0;
+}
