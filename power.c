c3c5584465337d63fc4b46f677153501096be990
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-02 16:22:01 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int base,exp;
+int base,exp,i;
 long long power=1
 printf("enter the base");
 scanf("%d",&base);
-printf("enter the exp);
+printf("enter the exp");
 scanf("%d",&exp);
 for(i=1;i<=exp;i++)
 {
 power=power*base;
 }
-printf("%d ^ %d=11%d",base,power,exp);
+printf("%d ^ %d=%d",base,exp,power);
 return 0;
 }
